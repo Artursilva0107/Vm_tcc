14faa1246d695fd308d89cb0f6ae32b4cda2c11b
Artursilva0107 <arturmwwp014@gmail.com>
2026-05-11 23:19:35 -0300

    Delete TCC directory

--- TCC/clientes/ver_empresa.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once '../includes/config.php';
-require_once '../includes/auth.php';
-verificarLogin();
-$id = (int)($_GET['id'] ?? 0);
-header($id ? "Location: empresa.php?id=$id" : "Location: empresas.php");
-exit;
